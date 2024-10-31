858f7494d3b6a934375ed788eba94a680b5cf9da
raihanghifariw <ghifariwinata@gmail.com>
2024-10-31 09:45:25 +0700

    Update aboutus.py

--- aboutus.py
+++ aboutus.py
@@ -52,7 +52,8 @@ def show():
     current_member = team_members[current_index]
 
     # Menampilkan informasi anggota tim saat ini
-    st.image(caption=current_member['name'], use_column_width=True)
+    # st.image(current_member['image'],
+    #          caption=current_member['name'], use_column_width=True)
     st.subheader(current_member['name'])
     st.write(current_member['description'])
 
