9b98f24476bee7a7b6be2b9c4cf697e29b68b33a
raihanghifariw <ghifariwinata@gmail.com>
2024-10-31 15:36:39 +0700

    Update app.py

--- app.py
+++ app.py
@@ -13,8 +13,9 @@ st.set_page_config(page_title="Algomind", page_icon="✨", layout="wide")
 menu = sidebar_menu()
 
 # Memanggil konten berdasarkan menu yang dipilih
-
-if menu == "Data":
+if menu == "Home":
+    home.show()
+elif menu == "Data":
     data.show()
 elif menu == "Visualisasi":
     visualisasi.show()
