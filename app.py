a28a0e4c55460f7f4109d8704a786a9713b18bbc
raihanghifariw <ghifariwinata@gmail.com>
2024-10-31 09:08:45 +0700

    test

--- app.py
+++ app.py
@@ -1,22 +1,18 @@
 import streamlit as st
 from sidebar import sidebar_menu
-import home
+import demo
 import data
 import visualisasi
-import demo
 import aboutus
-
 # Menggunakan sidebar untuk memilih menu
 menu = sidebar_menu()
 
 # Memanggil konten berdasarkan menu yang dipilih
-if menu == "Home":
-    home.show()
+if menu == "Demo":
+    demo.show()
 elif menu == "Data":
     data.show()
 elif menu == "Visualisasi":
     visualisasi.show()
-# elif menu == "Demo":
-#     demo.show()
 elif menu == "About Us":
     aboutus.show()
