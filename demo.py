58d914c8138470eb1669b547ef5d9b9bc8a7cd24
raihanghifariw <ghifariwinata@gmail.com>
2024-12-24 17:18:00 +0700

    bismillah

--- demo.py
+++ demo.py
@@ -114,7 +114,155 @@ import pandas as pd
 import numpy as np
 from itertools import product
 import torch
-from WD3QNE_deepQnet import WD3QNE
+import torch.nn as nn
+import torch.optim
+import torch.nn.functional as F
+import copy
+
+device = 'cpu'
+
+
+class WD3QNE_Net(nn.Module):
+    def __init__(self, state_dim, n_actions):
+        super(WD3QNE_Net, self).__init__()
+
+        self.conv = nn.Sequential(
+            nn.Linear(state_dim, 256),
+            nn.ReLU(),
+            nn.Linear(256, 256),
+            nn.ReLU(),
+            nn.Linear(256, 256),
+            nn.ReLU(),
+            nn.Linear(256, 256),
+            nn.ReLU(),
+        )
+        self.fc_val = nn.Sequential(
+            nn.Linear(128, 256),
+            nn.ReLU(),
+            nn.Linear(256, 1)
+        )
+        self.fc_adv = nn.Sequential(
+            nn.Linear(128, 256),
+            nn.ReLU(),
+            nn.Linear(256, n_actions)
+        )
+
+    def forward(self, state):
+        conv_out = self.conv(state)
+        val = self.fc_val(conv_out)
+        adv = self.fc_adv(conv_out)
+        return val + adv - adv.mean(dim=1, keepdim=True)
+
+    def get_q_values(self, state):
+        with torch.no_grad():
+            return self.forward(state)
+
+
+class WD3QNE:
+    def __init__(self,
+                 state_dim=37,
+                 num_actions=25,
+                 ensemble_size=3,
+                 gamma=0.99,
+                 tau=0.1):
+        self.device = device
+        self.ensemble_size = ensemble_size
+        self.num_actions = num_actions
+        self.gamma = gamma
+        self.tau = tau
+
+        # Ensemble Q-Networks
+        self.Q_ensemble = [WD3QNE_Net(state_dim, num_actions).to(device)
+                           for _ in range(ensemble_size)]
+        self.Q_target_ensemble = [copy.deepcopy(
+            q_net) for q_net in self.Q_ensemble]
+
+        self.optimizers = [torch.optim.Adam(q_net.parameters(), lr=0.0001)
+                           for q_net in self.Q_ensemble]
+
+    def train(self, batchs, epoch):
+        (state, next_state, action, next_action,
+         reward, done, bloc_num, SOFAS) = batchs
+        batch_s = 128
+        uids = np.unique(bloc_num)
+        num_batch = uids.shape[0] // batch_s
+        record_loss = []
+        sum_q_loss = 0
+        Batch = 0
+
+        for batch_idx in range(num_batch + 1):
+            batch_uids = uids[batch_idx * batch_s: (batch_idx + 1) * batch_s]
+            batch_user = np.isin(bloc_num, batch_uids)
+
+            # Ambil batch data
+            state_user = state[batch_user, :]
+            next_state_user = next_state[batch_user, :]
+            action_user = action[batch_user]
+            next_action_user = next_action[batch_user]
+            reward_user = reward[batch_user]
+            done_user = done[batch_user]
+            SOFAS_user = SOFAS[batch_user]
+
+            batch = (state_user, next_state_user, action_user,
+                     next_action_user, reward_user, done_user, SOFAS_user)
+
+            loss = self.compute_loss(batch)
+            sum_q_loss += loss
+            if Batch % 25 == 0:
+                print('Epoch :', epoch, 'Batch :', Batch,
+                      'Average Loss :', sum_q_loss / (Batch + 1))
+                record_loss1 = sum_q_loss / (Batch + 1)
+                record_loss.append(record_loss1)
+
+            if Batch % 100 == 0:
+                self.polyak_target_update()
+            Batch += 1
+
+        return record_loss
+
+    def compute_loss(self, batch):
+        state, next_state, action, next_action, reward, done, SOFA = batch
+        batch_size = state.shape[0]
+        end_multiplier = 1 - done
+        range_batch = torch.arange(batch_size).long().to(device)
+
+        total_loss = 0
+        for i, (q_net, q_target_net, optimizer) in enumerate(zip(self.Q_ensemble, self.Q_target_ensemble, self.optimizers)):
+            optimizer.zero_grad()
+
+            # Current Q-values
+            q_values = q_net(state)
+            q_value = q_values[range_batch, action]
+
+            # Next state Double Q-learning
+            with torch.no_grad():
+                q_values_next = torch.stack(
+                    [net.get_q_values(next_state) for net in self.Q_ensemble]).mean(0)
+                next_actions = q_values_next.argmax(dim=1)
+                q_target_next = q_target_net(next_state)
+                q_target_value = q_target_next[range_batch, next_actions]
+
+            target_q = reward + self.gamma * q_target_value * end_multiplier
+            loss = F.smooth_l1_loss(q_value, target_q)
+
+            loss.backward()
+            optimizer.step()
+            total_loss += loss.item()
+
+        return total_loss / self.ensemble_size
+
+    def polyak_target_update(self):
+        for q_net, q_target_net in zip(self.Q_ensemble, self.Q_target_ensemble):
+            for param, target_param in zip(q_net.parameters(), q_target_net.parameters()):
+                target_param.data.copy_(
+                    self.tau * param.data + (1 - self.tau) * target_param.data)
+
+    def get_action(self, state):
+        with torch.no_grad():
+            q_values = torch.stack([net.get_q_values(state)
+                                   for net in self.Q_ensemble]).mean(0)
+            return torch.argmax(q_values, dim=1)
+
 
 # Load pre-saved files
 DATA_PATH = "main/data/"
